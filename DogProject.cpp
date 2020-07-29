d96a031a861d619fc60e18b750afc10a759b68a1
Daniel Krigel <daniel.krigel@GMAIL.COM>
2020-07-29 22:17:57 +0300

    init

---
+++ DogProject.cpp
@@ -0,0 +1,12 @@
+﻿// DogProject.cpp : Defines the entry point for the application.
+//
+
+#include "DogProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
